e6d9e96271f91f882defbb4ec491ea24c6c9801f
Finall00 <104728826+finall00@users.noreply.github.com>
2025-03-18 19:07:51 -0300

    Update glpi_agent_inst.ps1

--- glpi_agent_inst.ps1
+++ glpi_agent_inst.ps1
@@ -1,5 +1,5 @@
-$GLPI_SERVER = "http://glpi.finall.space/front/inventory.php"
+$GLPI_SERVER = "https://glpi.finall.space/marketplace/glpiinventory"
 $LAB_NAME = "LAB-06"
 
 $installer = "GLPI-Agent-Installer.exe"
-Start-Process -FilePath $installer -ArgumentList "/S /server=$GLPI_SERVER /tag=$LAB_NAME" -Wait
+Start-Process -FilePath $installer -ArgumentList "/quiet SERVER=$GLPI_SERVER TAG=$LAB_NAME" -Wait
